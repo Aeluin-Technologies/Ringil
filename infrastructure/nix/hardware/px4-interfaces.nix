978b203eaf85efaa1c04eb820a9ad6c3732172d7
RealHinome <acn@gravitalia.com>
2026-04-22 13:08:25 +0200

    Remove comments

--- infrastructure/nix/hardware/px4-interfaces.nix
+++ infrastructure/nix/hardware/px4-interfaces.nix
@@ -1,10 +1,7 @@
 {
   services.udev.extraRules = ''
-    # Jetson UART (généralement ttyTHS0, ttyTHS1, etc.)
     KERNEL=="ttyTHS[0-9]*", GROUP="dialout", MODE="0660"
-    # Bus I2C pour des capteurs additionnels
     KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
-    # Bus SPI
     KERNEL=="spidev*", GROUP="spi", MODE="0660"
   '';
 
