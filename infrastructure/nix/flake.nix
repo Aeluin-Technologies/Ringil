7768ac2d3159859239129f76efc17fcef8bdefdb
RealHinome <acn@gravitalia.com>
2026-04-22 13:07:00 +0200

    Add all modules

--- infrastructure/nix/flake.nix
+++ infrastructure/nix/flake.nix
@@ -3,28 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
-    jetpack-nixos = {
-      url = "github:anduril/jetpack-nixos";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    jetpack-nixos.url = "github:anduril/jetpack-nixos";
+    jetpack-nixos.inputs.nixpkgs.follows = "nixpkgs";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, jetpack-nixos, disko, ... }@inputs: let
-    mkDrone = { hostname, system ? "aarch64-linux", profile }: 
+  outputs = {
+    self,
+    nixpkgs,
+    jetpack-nixos,
+    disko,
+    ...
+  } @ inputs: let
+    system = "aarch64-linux";
+
+    mkDrone = {
+      hostname,
+      profile,
+    }:
       nixpkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = { inherit inputs; };
+        specialArgs = {inherit inputs;};
         modules = [
           disko.nixosModules.disko
           jetpack-nixos.nixosModules.default
 
           ./modules/core/env.nix
+          ./modules/core/bootloader.nix
+          ./modules/core/filesystems.nix
+          ./hardware/jetson.nix
+          ./hardware/px4-interfaces.nix
+          ./hardware/cuda-tensorrt.nix
+          ./modules/security/anssi-kernel.nix
+          ./modules/security/users.nix
+          ./modules/network/galadril-link.nix
+          ./modules/observability/metrics.nix
+          ./modules/ringil/service.nix
+
+          ./profiles/${profile}.nix
+          {networking.hostName = hostname;}
         ];
       };
   in {
@@ -33,11 +51,13 @@
         hostname = "dev-drone-01";
         profile = "dev";
       };
-
       "prod-swarm-alpha" = mkDrone {
         hostname = "prod-swarm-alpha";
         profile = "prod";
       };
     };
+
+    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
   };
 }
