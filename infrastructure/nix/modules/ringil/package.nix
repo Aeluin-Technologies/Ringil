84eba2780ec443d5c9d1ed06289b2b2124e346d0
RealHinome <acn@gravitalia.com>
2026-04-27 23:28:19 +0200

    Add orb-slam3 hash for reproducible builds

--- infrastructure/nix/modules/ringil/package.nix
+++ infrastructure/nix/modules/ringil/package.nix
@@ -10,6 +10,7 @@ pkgs.rustPlatform.buildRustPackage {
     outputHashes = {
       "ort-2.0.0-rc.12" = "sha256-BptpN7BO5FVO1znc01YXuWIkLn1H5bSuJCDSJOdJNFg=";
       "ultralytics-inference-0.0.10" = "sha256-7P6v/ZjJ3SKq/2YqefgClmzIx2NAyOf5tilPFwHAnpo=";
+       "orb-slam3-0.1.0" = "sha256-6CINKdSMuAdDqklfu0CXsALbOQKzvN5vi7xpg2690N4=";
     };
   };
 
