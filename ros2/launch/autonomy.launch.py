4f4b2137658016db85fd6079d568112cf5705448
RealHinome <acn@gravitalia.com>
2026-05-19 11:57:11 +0200

    Add ROS2 launch script

---
+++ ros2/launch/autonomy.launch.py
@@ -0,0 +1,52 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    package_name = "ringil_bringup"
+    config_dir = os.path.join(get_package_share_directory(package_name), "config")
+
+    nav2_yaml = os.path.join(config_dir, "nav2.yaml")
+    rtabmap_yaml = os.path.join(config_dir, "rtabmap.yaml")
+    nvblox_yaml = os.path.join(config_dir, "nvblox.yaml")
+    isaac_vslam_yaml = os.path.join(config_dir, "isaac_ros_vslam.yaml")
+
+    return LaunchDescription(
+        [
+            Node(
+                package="isaac_ros_visual_slam",
+                executable="isaac_ros_visual_slam",
+                name="visual_slam_node",
+                parameters=[isaac_vslam_yaml],
+                remappings=[("visual_slam/odom", "/visual_odom")],
+            ),
+            Node(
+                package="rtabmap_slam",
+                executable="rtabmap",
+                name="rtabmap",
+                parameters=[rtabmap_yaml],
+                arguments=["-d"],  # Delete previous database on start.
+            ),
+            Node(
+                package="nvblox_ros",
+                executable="nvblox_node",
+                name="nvblox_node",
+                parameters=[nvblox_yaml],
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory("nav2_bringup"),
+                        "launch",
+                        "navigation_launch.py",
+                    )
+                ),
+                launch_arguments={"params_file": nav2_yaml}.items(),
+            ),
+        ]
+    )
