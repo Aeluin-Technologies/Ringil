2e658023eae2d546232e53cce643268350866b28
RealHinome <acn@gravitalia.com>
2026-05-20 17:30:48 +0200

    Improve ROS2 on Nix

--- infrastructure/nix/modules/ringil/ros2.nix
+++ infrastructure/nix/modules/ringil/ros2.nix
@@ -3,21 +3,73 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  rosDistro = pkgs.rosPackages.jazzy;
+
+  ringilBringup = pkgs.callPackage ./ringil-bringup.nix {
+    buildRosPackage = rosDistro.buildRosPackage;
+    ament-python = rosDistro.ament-python;
+    launch = rosDistro.launch;
+    launch-ros = rosDistro.launch-ros;
+  };
+
+  autonomyLaunchScript = pkgs.writeScriptBin "ringil-ros2-launch" ''
+    #!${pkgs.stdenv.shell}
+    source ${rosDistro.ros-workspace}/setup.bash
+    cleanup() {
+      kill $AGENT_PID || true
+    }
+    trap cleanup EXIT
+    MicroXRCEAgent serial --dev /dev/ttyTHS0 -b 921600 &
+    AGENT_PID=$!
+    sleep 1
+    exec ros2 launch ringil_bringup autonomy.launch.py
+  '';
+in {
   environment.variables = {
     ROS_DOMAIN_ID = "42";
     RMW_IMPLEMENTATION = "rmw_zenoh_cpp";
-    # ROS_PARAMS_FILE = "/etc/ringil/config/high_speed.yaml";
   };
 
-  environment.systemPackages = with pkgs; [
-    rosPackages.jazzy.ros-core
-    rosPackages.jazzy.rmw-zenoh-cpp
+  environment.systemPackages = with rosDistro; [
+    ros-core
+    rmw-zenoh-cpp
 
-    rosPackages.jazzy.navigation2
-    rosPackages.jazzy.nav2-bringup
-    rosPackages.jazzy.behaviortree-cpp
+    rosDistro.isaac-ros-visual-slam
+    rosDistro.isaac-ros-nvblox
 
-    rosPackages.jazzy.ros2cli
+    rosDistro.aerostack2
+    rosDistro.as2-core
+    rosDistro.as2-motion-controller
+    rosDistro.as2-state-estimator
+    rosDistro.as2-platform-pixhawk
+
+    pkgs.micro-xrce-dds-agent
+
+    ringilBringup
+    autonomyLaunchScript
   ];
+
+  systemd.services.ringil-ros2 = {
+    description = "Ringil ROS 2 Autonomy Stack";
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
+
+    environment = {
+      ROS_DOMAIN_ID = "42";
+      RMW_IMPLEMENTATION = "rmw_zenoh_cpp";
+    };
+
+    serviceConfig = {
+      ExecStart = "${autonomyLaunchScript}/bin/ringil-ros2-launch";
+      Restart = "always";
+      RestartSec = "1s";
+      User = "ringil";
+      Group = "ringil";
+
+      LimitRTPRIO = 99;
+      CPUSchedulingPolicy = "fifo";
+      CPUSchedulingPriority = 80;
+    };
+  };
 }
